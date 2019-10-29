4fafa6a12b9b1fd38185d6b3ce77fc67a9b9ef9b
fosemberg <fosemberg@gmail.com>
2019-10-29 14:30:32 +0300

    Расчет импульса: calcVolume

--- src/helpers/computeMomentum.js
+++ src/helpers/computeMomentum.js
@@ -1,4 +1,4 @@
-import {prop, cond, compose} from 'ramda';
+import {prop, cond, pipe, equals} from 'ramda';
 
 import {SHAPES} from '../constants';
 
@@ -6,8 +6,8 @@ import {SHAPES} from '../constants';
 const round = num => Math.round(num * 10) / 10;
 
 /**
- * Функции возвращающие св-ва объекта эквивалент 
- * 
+ * Функции возвращающие св-ва объекта эквивалент
+ *
  * const propShape = obj => obj.shape
  **/
 const propShape = prop('shape');
@@ -23,7 +23,7 @@ const G = 9.8;
 
 const momentumFormula = ({mass, velocity}) => mass * velocity;
 
-const velocityFormula = height => Math.sqrt(2 * G * height); 
+const velocityFormula = height => Math.sqrt(2 * G * height);
 
 const massFormula = ({volume, density}) => volume * density;
 
@@ -33,53 +33,47 @@ const sphereVolumeFormula = d => (Math.PI * Math.pow(d, 3)) / 6;
 
 const tetrahedronVolumeFormula = r => (Math.pow(r, 3) * Math.sqrt(2)) / 12;
 
-
-const shapeEqualsCube = compose(
-    () => {},
-    //
+const shapeEquals = shape => pipe(
+  propShape,
+  equals(shape),
 );
 
-const shapeEqualsSphere = compose(
-    () => {},
-    //
-);
-const shapeEqualsTetrahedron = compose(
-    () => {},
-    //
+const shapeEqualsCube = shapeEquals(SHAPES.CUBE);
+const shapeEqualsSphere = shapeEquals(SHAPES.SPHERE);
+const shapeEqualsTetrahedron = shapeEquals(SHAPES.TETRAHEDRON);
+
+const calcVolumeWithFormula = formula => pipe(
+  propHeight,
+  formula,
 );
 
-const calcCubeVolume = compose(
-    () => {},
-    //
-);
-const calcSphereVolume = compose(
-    () => {},
-    //
-);
-const calcTetrahedronVolume = compose(
-    () => {},
-    //
-);
+const calcCubeVolume = calcVolumeWithFormula(cubeVolumeFormula);
+const calcSphereVolume = calcVolumeWithFormula(sphereVolumeFormula);
+const calcTetrahedronVolume = calcVolumeWithFormula(tetrahedronVolumeFormula);
 
 const calcVolume = cond([
-    [() => {}, () => {}],
-    [() => {}, () => {}]
-    //
+    [ shapeEqualsCube, calcCubeVolume],
+    [ shapeEqualsSphere, calcSphereVolume],
+    [ shapeEqualsTetrahedron, calcTetrahedronVolume],
 ]);
 
-const calcMass = compose(
+const calcMass = pipe(
     () => {},
     //
 );
 
-const calcVelocity = compose(
+const calcVelocity = pipe(
     () => {},
     //
 );
 
-const computeMomentum = compose(
-    () => {},
-    //
+const computeMomentum = pipe(
+    // calcVolume(propSize(props), propShape(props)),
+    // () => {console.log('Height:', propHeight(props))},
+    // () => {console.log('Size:', propSize(props))},
+    // () => {console.log('Shape:', propShape(props))},
+    // () => {console.log('Density:', propDensity(props))},
+    calcVolume,
 );
 
 
